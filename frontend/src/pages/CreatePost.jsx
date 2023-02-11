4c633201b50a2fde2287becb8d3e395e72bc171f
talhayavcin <talhayavcin35@gmail.com>
2023-02-11 14:27:55 +0300

    last changes

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -40,8 +40,29 @@ const CreatePost = () => {
     }
   }
 
-  const handleSubmit = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    if (form.prompt && form.photo) {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify(form)
+        })
+
+        await response.json();
+        navigate('/');
+      } catch (err) {
+        alert(err)
+      } finally {
+        setLoading(false);
+      }
+    }
   }
 
   const handleChange = (e) => {
